275846e78314bd535f548416b361487744282b2b
yunus deemad <yunuszero@hotmail.com>
2020-08-11 22:18:49 +0700

    complete the beta source code using array.

--- Challenge1/Challenge1.cpp
+++ Challenge1/Challenge1.cpp
@@ -1,6 +1,52 @@
 #include<stdio.h>
+#include<math.h>
+
+double FindAverage(double* x,int n)
+{
+	double sum = 0;
+	for (int i = 0; i < n; i++) {
+		sum += x[i];
+	}
+	return sum / n;
+}
+
+double FindSD(double* x,int n)
+{
+	if (n == 1) return 0;
+	double sum = 0;
+	for (int i = 0; i < n; i++) {
+		sum += pow(fabs(x[i] - FindAverage(x,n)), 2);
+	}
+	return sqrt(sum / (n - 1));
+}
+
 int main()
 {
+	int n, m;
+	double average, sd;
+	scanf_s("%d %d", &n, &m);
+	double arr[1010];
+	if (n < m)
+	{
+		for (int i = n; i <= m ; i++)
+		{
+			printf("%d ", i);
+			arr[i - n] = i;
+		}
+	}
+	else
+	{
+		for (int i = n; i >= m; i--)
+		{
+			printf("%d ", i);
+			arr[n - i] = i;
+		}
+	}
+	printf("\n");
+	average = FindAverage(arr, abs(n - m) + 1);
+	sd = FindSD(arr, abs(n - m) + 1);
+	printf("average = %.1lf\n", average);
+	printf("SD = %.2lf", sd);
 
 	return 0;
 }
